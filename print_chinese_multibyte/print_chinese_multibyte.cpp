2ceb7863b2a6ae49cadb9f508aa983c3e90a15fc
SulfredLee <sflee1112@gmail.com>
2016-08-07 11:11:10 +0800

    print_chinese_multibyte, now can print chinese

--- print_chinese_multibyte/print_chinese_multibyte.cpp
+++ print_chinese_multibyte/print_chinese_multibyte.cpp
@@ -4,6 +4,10 @@
 #include "stdafx.h"
 #include "print_chinese_multibyte.h"
 
+#include <iostream>
+#include <string>
+#include <locale>
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -18,6 +22,12 @@ using namespace std;
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	int nRetCode = 0;
+
+	locale loc("cht");
+	std::wcout.imbue(loc);
+	std::wstring strTemp = L"text§A¦n";
+	std::wcout << strTemp << std::endl;
+	system("pause");
 
 	return nRetCode;
 }
