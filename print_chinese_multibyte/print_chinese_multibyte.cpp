f9d616bfcdb23a09bf33cc38ae68b551f48aeb69
SulfredLee <sflee1112@gmail.com>
2016-08-07 11:09:10 +0800

    print_chinese_multibyte, start

---
+++ print_chinese_multibyte/print_chinese_multibyte.cpp
@@ -0,0 +1,23 @@
+// print_chinese_multibyte.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "print_chinese_multibyte.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	return nRetCode;
+}
