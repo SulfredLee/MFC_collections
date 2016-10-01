450fbb75c9a50dd95dcf86a8d971ed865608c90c
ShiuFai <ShiuFai@SULFRED-PC>
2016-10-01 21:13:01 +0800

    removed a project dynamic_controls_within_list_control

--- dynamic_controls_within_list_control/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// dynamic_controls_within_list_control.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
